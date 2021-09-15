38b9dfc6d6d88e9cb6f8bb2dd6ad5919cef956ad
valerio <romavalerioio@gmail.com>
2021-09-15 12:23:50 +0100

    display component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Flight from "../Flight";
+import Display from "../Display";
 
 function App() {
   return (
     <div>
-      <Flight dateTime='20:00 15-09-2021' departureAirport='London Heathrow' arrivalAirport='Dulles' airline='British Airways' flightNumber='5' changes={{numberOfStops: 1, intermediateAirports: {iata: [{station: 'Germany'}]}}} isElectronicTicketing={true} isAutomatedCheckin={false} />
+    <Display flights={[{dateTime:'20:00 15-09-2021', departureAirport:'London Heathrow', arrivalAirport:'Dulles', airline:'British Airways', flightNumber:'5', changes:{numberOfStops: 1, intermediateAirports: {iata: [{station: 'Germany'}]}}, isElectronicTicketing:true, isAutomatedCheckin:false}]}/>
     </div>
   );
 }
--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,1 +1,36 @@
- 
+import React from "react";
+import Flight from '../Flight'
+import {v4 as uuidv4} from 'uuid';
+
+function Display({ flights }) {
+  return (
+    <section>
+      {flights.map(
+        ({
+          dateTime,
+          departureAirport,
+          arrivalAirport,
+          airline,
+          flightNumber,
+          changes,
+          isElectronicTicketing,
+          isAutomatedCheckin,
+        }) => (
+          <Flight
+            key={uuidv4()}
+            dateTime={dateTime}
+            departureAirport={departureAirport}
+            arrivalAirport={arrivalAirport}
+            airline={airline}
+            flightNumber={flightNumber}
+            changes={changes}
+            isElectronicTicketing={isElectronicTicketing}
+            isAutomatedCheckin={isAutomatedCheckin}
+          />
+        )
+      )}
+    </section>
+  );
+}
+
+export default Display;
