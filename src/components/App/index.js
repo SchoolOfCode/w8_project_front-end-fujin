6f6a0ebe1cdc3d5833d115b00eb637b28ce8c178
tzuyunlin <tzuyunlin.tw@gmail.com>
2021-09-15 15:47:53 +0100

    moved the departure state to the App index

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Form from '../Form'
+import Form from "../Form";
 
 function App() {
+  const [departurePort, setDeparturePort] = useState({ name: " ", code: " " });
+
+  function chosenDepAirport(portName, portCode) {
+    setDeparturePort({ ...departurePort, name: portName, code: portCode });
+    // console.log(departurePort);
+  }
 
   return (
     <div>
       <header>Swifter</header>
       <h3>
         Departure
-        <Form id="depart"/>
+        <Form
+          id="depart"
+          state={departurePort}
+          chosenAirport={chosenDepAirport}
+        />
       </h3>
 
       <br></br>
 
       <h3>
         Arrival
-        <Form id="arrive"/>
+        <Form id="arrive" />
       </h3>
-      
-    
     </div>
   );
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,49 +1,53 @@
-import {useState} from "react";
+import { useState } from "react";
 // import button component here
 
-function Form () {
+function Form({ state, chosenAirport }) {
+  const [city, setCity] = useState("");
+  const [airports, setAirports] = useState([]);
 
-    const [city, setCity] = useState("")
-    const [airports, setAirports] = useState([])
-    const [chosen, setChosen] = useState("")
-    const [code, setCode] = useState("")
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
 
-    function handleChange(event) {
-        setCity(event.target.value)
-    }
+  function addAirports(payload) {
+    setAirports(payload);
+    console.log(airports);
+  }
 
-    function addAirports(payload) {
-        setAirports(payload)
-        console.log(airports)
-    }
+  async function getAiportByCity(city) {
+    const response = await fetch(`http://localhost:5000/airports?city=${city}`);
+    const { payload } = await response.json();
+    console.log(payload);
+    addAirports(payload);
+  }
+  console.log(state);
 
-    async function getAiportByCity(city) {
-        const response = await fetch (`http://localhost:5000/airports?city=${city}`)
-        const {payload} = await response.json()
-        console.log(payload)
-        addAirports(payload)
-    }
-       
-    function chosenAirport(name, code) {
-        setChosen(name)
-        setCode(code)
-    }
-
-    return (
-        <section>            
-            <input type="text" name="city" placeholder="Enter Departure City" onChange={handleChange} value={city} required ></input>
-            <button onClick={()=>getAiportByCity(city)}>Show Airports</button>
-            {airports.map(item => {
-                return <li onClick={()=>chosenAirport(item.airport_name, item.airport_code)}>{item.airport_name}, {item.city_name}</li>
-            })}
-            <h5>You have chosen {chosen}</h5>
-            <h5>code {code}</h5>
-       
-        </section>
-    )
+  return (
+    <section>
+      <input
+        type="text"
+        name="city"
+        placeholder="Enter Departure City"
+        onChange={handleChange}
+        value={city}
+        required
+      ></input>
+      <button onClick={() => getAiportByCity(city)}>Show Airports</button>
+      {airports.map((item) => {
+        return (
+          <li
+            onClick={() => chosenAirport(item.airport_name, item.airport_code)}
+          >
+            {item.airport_name}, {item.city_name}
+          </li>
+        );
+      })}
+      {state ? <h5>You have chosen {state.name}</h5> : ""}
+    </section>
+  );
 }
 
-export default Form
+export default Form;
 
 // create a state for the city
 // create a handle change function to set the state on change. this uses target.value
@@ -51,6 +55,6 @@ export default Form
 // set the valuye of the input tag to be the state
 
 // we want to display airport names from the payload and country and city
-// get payload from the backend fetch 
+// get payload from the backend fetch
 // go through each object in the array and take out the info we need
 // render each airport as a radio buttons
