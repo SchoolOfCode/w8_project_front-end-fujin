dff396b1da1b6586e19186c9fd32f993e0cef804
himself949 <doubleh290986@hotmail.com>
2021-09-15 12:34:15 +0100

    departure form under construction with airports

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,29 @@
+import {useState} from "react";
 import "./App.css";
 import Form from '../Form'
 
 function App() {
+  
+  const [departCity, setDepartCity] = useState("")
+
+  async function getAiportByCity(city) {
+    const response = await fetch (`http://localhost:5000/airports?city=${city}`)
+    const {payload} = await response.json()
+    console.log(payload)
+    return payload
+}
+
   return (
     <div>
       <header>Swifter</header>
-      <Form/>
+
+      <DepartForm />
+      {list component}
+
+      <ArriveForm/>
+      {list component}
+
+      {date thing}
     </div>
   );
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,14 +3,19 @@ import {useState} from "react";
 
 function Form () {
     
+    /*
     const [city, setCity] = useState("")
+
 
     async function getAiportByCity(city) {
         const response = await fetch (`http://localhost:5000/airports?city=${city}`)
         const {payload} = await response.json()
         console.log(payload)
-        // return payload
+        return payload
     }
+    */
+
+
 
     function handleChange(event) {
         setCity(event.target.value)
@@ -21,9 +26,8 @@ function Form () {
             <label >Departure City</label>
             <input type="text" name="city" placeholder="Enter Departure City" onChange={handleChange} value={city} required ></input>
             <button onClick={()=>getAiportByCity(city)}>Show Airports</button>
-            <br></br>
-            <label>Departure Date</label>
-            <input type="date"></input>          
+            
+       
         </section>
     )
 }
@@ -34,3 +38,8 @@ export default Form
 // create a handle change function to set the state on change. this uses target.value
 // in the input tag, we have an onchange property that ruyns the handle change function
 // set the valuye of the input tag to be the state
+
+// we want to display airport names from the payload and country and city
+// get payload from the backend fetch 
+// go through each object in the array and take out the info we need
+// render each airport as a radio buttons
